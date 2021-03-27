b89ab2ec18241ad445cd9a883ad73ecf2638455c
Tabata Sutili <ssabrinasutili@gmail.com>
2021-03-27 20:55:36 -0300

    .

---
+++ AppFilmes/Pods/Target Support Files/Pods-AppFilmes-AppFilmesUITests/Pods-AppFilmes-AppFilmesUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AppFilmes_AppFilmesUITests : NSObject
+@end
+@implementation PodsDummy_Pods_AppFilmes_AppFilmesUITests
+@end
---
+++ AppFilmes/Pods/Target Support Files/Pods-AppFilmes/Pods-AppFilmes-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AppFilmes : NSObject
+@end
+@implementation PodsDummy_Pods_AppFilmes
+@end
